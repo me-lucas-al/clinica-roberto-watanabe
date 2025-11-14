27c4d56fe582975a421835082a49a9a2de9dfccc
me-lucas-al <me.lucasalmeida@gmail.com>
2025-11-14 15:02:30 -0300

    refactor: separating frontend and backend folder

--- src/Components/Footer/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './footer.module.css'
-
-export default function Footer() {
-  return (
-    <div className={styles.footer}>
-      © 2025 Roberto Watanabe. Todos os direitos reservados.
-    </div>
-  )
-}
