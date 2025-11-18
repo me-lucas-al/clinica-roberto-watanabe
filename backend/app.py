0a20389648c0c19be7c24a11025a549a935ef4e9
me-lucas-al <me.lucasalmeida@gmail.com>
2025-11-18 17:01:43 -0300

    feat: configurate request data

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify
+from backend.models import models
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from models import Usuario
 
 app = Flask(__name__)
 
@@ -9,9 +11,25 @@ CORS(app)
 def home():
     return "Welcome to the Home Page!"
 
-@app.route('/api/data')
-def get_data():
-    return jsonify({'message': 'Dados do Flask recebidos no React!'})
+@app.route('/api/register', methods=['POST'])
+def register():
+
+    data = request.get_json()
+    
+    required_fields = ['nomeCompleto', 'senha', 'email', 'tipoUsuario', 'dataNasc']
+    for field in required_fields:
+        if field not in data:
+            return jsonify({'success': False, 'message': f'Campo obrigatório ausente: {field}'}), 400
+    
+    novo_usuario = Usuario(
+        nomeCompleto=data['nomeCompleto'],
+        dataNasc=data_nascimento,
+        senha_hash=hashed_password,
+        email=email,
+        telefone=data.get('telefone'), # Usa .get() para campos opcionais
+        tipoUsuario=data['tipoUsuario']
+    )
+    return jsonify({"message": "User registered successfully!"}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
