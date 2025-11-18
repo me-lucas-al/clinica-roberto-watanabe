fc363eeac53bbbd2e700f4d05e6a0b5038278183
me-lucas-al <me.lucasalmeida@gmail.com>
2025-11-18 15:35:57 -0300

    feat: configurate api communication

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/')
 def home():
     return "Welcome to the Home Page!"
+
+@app.route('/api/data')
+def get_data():
+    return jsonify({'message': 'Dados do Flask recebidos no React!'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
