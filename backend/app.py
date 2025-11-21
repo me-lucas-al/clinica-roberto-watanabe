1047b2a15b57ded2a4ce0c3b390ccc7e3b63f8e2
me-lucas-al <me.lucasalmeida@gmail.com>
2025-11-21 14:33:56 -0300

    feat: instancing Agendamento and  Usuario tables

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 from backend.models import models
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, session
 from flask_cors import CORS
-from models import Usuario
+from backend.models import Usuario
+from backend.models import Agendamento
 
 app = Flask(__name__)
 
 CORS(app)
+
 
 @app.route('/')
 def home():
@@ -23,13 +25,36 @@ def register():
     
     novo_usuario = Usuario(
         nomeCompleto=data['nomeCompleto'],
-        dataNasc=data_nascimento,
-        senha_hash=hashed_password,
-        email=email,
-        telefone=data.get('telefone'), # Usa .get() para campos opcionais
+        dataNasc=data['dataNasc'],
+        senha_hash=data['senha'],
+        email=data['email'],
+        telefone=data['telefone'],
         tipoUsuario=data['tipoUsuario']
     )
+
+    session.add(novo_usuario)
+    session.commit()
+
     return jsonify({"message": "User registered successfully!"}), 201
+
+@app.route('/api/criar-agendamento', methods=['POST'])
+def criar_agendamento():
+    data = request.get_json()
+
+    required_fields = ['dataAgendamento', 'horaAgendamento', 'usuarioId']
+    for field in required_fields:
+        if field not in data:
+            return jsonify({'success': False, 'message': f'Campo obrigatório ausente: {field}'}), 400
+    
+    novo_agendamento = Agendamento(
+        dataAgendamento=data['dataAgendamento'],
+        horaAgendamento=data['horaAgendamento'],
+        usuarioId=data['usuarioId']
+    )
+    session.add(novo_agendamento)
+    session.commit()
+
+    return jsonify({"message": "Agendamento criado com sucesso!"}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
