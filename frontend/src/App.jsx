dac38fd7a3aebe6e82c1a4199f10f9c102ed6342
me-lucas-al <me.lucasalmeida@gmail.com>
2025-12-01 17:04:04 -0300

    feat: create perfil page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Home from "./pages/Home"
 import Login from "./pages/Login/Login";
 import Cadastro from "./pages/Cadastro/Cadastro";
+import Perfil from "./pages/Perfil/Perfil.jsx";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/perfil" element={<Perfil />} />
       </Routes>
     </BrowserRouter>
   );
